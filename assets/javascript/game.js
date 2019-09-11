65ef54cc09a8379ea14d8a24bb856d76d61a70a6
Monica Bongue <muddyforkfarm@gmail.com>
2019-09-10 20:11:06 -0400

    add try again button and counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,9 @@ var purpleGuess;
 var orangeGuess;
 var blueGuess;
 var totalScore = 0;
+var wins = 0;
+var loses = 0;
+
 
 //assign this number to magic numer
 //generate randon number between 1 to 12
@@ -17,13 +20,17 @@ $(document).ready(function() {
     function compareScore() {
 
         if (totalScore === magicNumber) {
-
+            wins++;
             console.log("You win");
-            $("#wins").text("you win !");
+            $("#wins").text("you win; " + wins);
+            reset();
+
 
         } else if (totalScore > magicNumber) {
+            loses++;
             console.log("you lose");
-            $("#loses").text("Sorry, Try Again");
+            $("#loses").text("Sorry, Try Again, You lost " + loses + " times");
+            reset();
 
         } else {
             //console.log(totalScore);
@@ -32,7 +39,10 @@ $(document).ready(function() {
 
     }
 
-    function keepTrack() {
+    function reset() {
+        generateNumbers()
+        totalScore = 0;
+
 
     }
 
